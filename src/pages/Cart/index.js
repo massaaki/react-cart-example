7918f24ce4a04b6ad9fa4b152c54ca02f37cd73a
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-04-28 17:52:09 -0300

    adding cart index layout

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th> </th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-feminino/34/D16-2249-234/D16-2249-234_zoom2.jpg?ts=1587439510&ims=120x"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <span>R$ 120,00</span>
+            </td>
+
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$ 253,90</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button"> Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1234,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
