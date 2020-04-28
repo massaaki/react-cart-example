af3d7cf29a261616db5747a4c917cc01448ab47b
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-04-28 17:22:58 -0300

    adding home layout

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,102 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-feminino/34/D16-2249-234/D16-2249-234_zoom2.jpg?ts=1587439510&ims=120x"
+          alt="tenis"
+        />
+        <strong>Tênis Mizuno Jet 3 N Feminino - Preto e Rosa</strong>
+        <span>R$ 120,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-feminino/34/D16-2249-234/D16-2249-234_zoom2.jpg?ts=1587439510&ims=120x"
+          alt="tenis"
+        />
+        <strong>Tênis Mizuno Jet 3 N Feminino - Preto e Rosa</strong>
+        <span>R$ 120,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-feminino/34/D16-2249-234/D16-2249-234_zoom2.jpg?ts=1587439510&ims=120x"
+          alt="tenis"
+        />
+        <strong>Tênis Mizuno Jet 3 N Feminino - Preto e Rosa</strong>
+        <span>R$ 120,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-feminino/34/D16-2249-234/D16-2249-234_zoom2.jpg?ts=1587439510&ims=120x"
+          alt="tenis"
+        />
+        <strong>Tênis Mizuno Jet 3 N Feminino - Preto e Rosa</strong>
+        <span>R$ 120,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-feminino/34/D16-2249-234/D16-2249-234_zoom2.jpg?ts=1587439510&ims=120x"
+          alt="tenis"
+        />
+        <strong>Tênis Mizuno Jet 3 N Feminino - Preto e Rosa</strong>
+        <span>R$ 120,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-feminino/34/D16-2249-234/D16-2249-234_zoom2.jpg?ts=1587439510&ims=120x"
+          alt="tenis"
+        />
+        <strong>Tênis Mizuno Jet 3 N Feminino - Preto e Rosa</strong>
+        <span>R$ 120,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
