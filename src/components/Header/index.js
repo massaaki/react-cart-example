2ee2b2e37dccb3486d58563d1f1a29409d82f037
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-04-29 16:10:22 -0300

    redux convert

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { MdShoppingBasket } from 'react-icons/md';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
-function Header({ cartSize }) {
+export default function Header() {
+  const cartSize = useSelector( state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
@@ -24,7 +26,3 @@ function Header({ cartSize }) {
   );
 }
 
-
-export default connect(state => ({
-  cartSize: state.cart.length,
-}))(Header);
