e0a84b3925e5dbb0494d4cbc6462f18954ce411d
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-04-29 10:29:24 -0300

    adjust name

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,14 @@
  *
  */
 import { createStore, applyMiddleware, compose } from 'redux';
-import createsagaMiddleware from 'redux-saga';
+import createSagaMiddleware from 'redux-saga';
 
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
 
-const sagaMiddleware = createsagaMiddleware();
+const sagaMiddleware = createSagaMiddleware();
 
 const enhancer = process.env.NODE_ENV === 'development'
   ? compose(console.tron.createEnhancer(), applyMiddleware(sagaMiddleware))  : applyMiddleware(sagaMiddleware);
